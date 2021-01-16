a2640e0a94c1992035015c03b2e2411cd5a2db0b
Pubudu Gunatilaka <pubudu538@gmail.com>
2021-01-16 17:37:00 +0530

    Addin API and MI

---
+++ mi-artifacts/eventapp/build.sh
@@ -0,0 +1,5 @@
+gradle build
+cd build/distributions/
+unzip eventapp-1.0.zip
+cd ../../
+cp build/distributions/eventapp-1.0/lib/eventapp-1.0.jar /Applications/IntegrationStudio.app/Contents/Eclipse/runtime/microesb/lib/
